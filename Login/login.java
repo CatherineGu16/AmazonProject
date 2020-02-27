f825b143d2255e02c086b906d7c33c42be91761a
Anthony Chin <Anthony Chin@DESKTOP-A7GSP2S>
2020-02-27 10:55:49 -0800

    Fixed?

--- Login/login.java
+++ Login/login.java
@@ -1,5 +1,12 @@
 package Login;
+import javax.swing.JTextField;
+
 import UI.*;
 public class login {
+	public static void main(String[] args) {
+
+		
+		System.out.println("John Mortensen");
+	}
 	 //
 }
