497e17c7900b8bd1499fb8167def4b8e3be9f5b6
CatherineGu <catherine8gu9@gmail.com>
2020-02-27 11:10:28 -0800

    resolved conflicts

--- Login/login.java
+++ Login/login.java
@@ -1,20 +1,10 @@
 package Login;
-<<<<<<< HEAD
+
 import javax.swing.JTextField;
-=======
 import java.util.*;
->>>>>>> branch 'master' of https://github.com/CatherineGu16/AmazonProject.git
-
 import UI.*;
-<<<<<<< HEAD
-public class login {
-	public static void main(String[] args) {
 
-		
-		System.out.println("John Mortensen");
-	}
-	 //
-=======
+
 
 public class login 
 {
@@ -41,5 +31,5 @@ public class login
 		}
 		
 	}
->>>>>>> branch 'master' of https://github.com/CatherineGu16/AmazonProject.git
+
 }
